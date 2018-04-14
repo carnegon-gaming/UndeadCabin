d70b5e555073785334d05a578bdc0ebd40cbe60d
yuval alter <carnegon11@gmail.com>
2018-04-14 18:19:50 +0300

    added minimap
    MINIMAP!!!!!!!!!

---
+++ UndeadCabin/Assets/Scripts/Minimap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour {
+
+    public Transform Player;
+
+	private void FixedUpdate()
+	{
+        Vector3 newPosition = Player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+	}
+
+}
