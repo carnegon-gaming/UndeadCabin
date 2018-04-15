3554aa51653df297ae88750c83ac9d491e1f3a1b
yuval alter <carnegon11@gmail.com>
2018-04-15 21:42:54 +0300

    Gun Sound added
    pew pew pew pew!!!!

---
+++ UndeadCabin/Assets/Scripts/GunSound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunSound : MonoBehaviour
+{
+    private AudioSource mySound;
+    public AudioClip soundOn;
+
+    // Use this for initialization
+    void Start()
+    {
+        mySound = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            mySound.clip = soundOn;
+            mySound.Play();
+        }
+    }
+}
